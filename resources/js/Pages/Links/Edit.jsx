285118a5e620e2c4130aaf611b73f7c8b5d99eed
Brad Felix <bradfelix1@gmail.com>
2022-09-15 18:11:16 -0400

    cleaned up a little

--- resources/js/Pages/Links/Edit.jsx
+++ resources/js/Pages/Links/Edit.jsx
@@ -21,7 +21,7 @@ export default function Dashboard(props) {
             errors={props.errors}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Edit Link</h2>}
         >
-            <Head title="Posts" />
+            <Head title="Links" />
   
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
